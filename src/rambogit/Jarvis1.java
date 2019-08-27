49638b97df99f7a0c05a09905eed4c96811a03b7
trippytester <amitabh.nampalliwar01@gmail.com>
2019-08-27 23:20:00 +0530

    yeyyyyyyyy

---
+++ src/rambogit/Jarvis1.java
@@ -0,0 +1,14 @@
+package rambogit;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jarvis1 {
+	
+	@Test
+	public void get2()
+	{
+		Reporter.log("hey",true);
+	}
+
+}
